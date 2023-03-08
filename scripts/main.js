cfc79a3936e3a560d67ab7f74f6ade97e335ca42
Axelerations <91915412+Axelerations@users.noreply.github.com>
2023-03-08 17:59:17 -0400

    Squashed commit of the following:
    commit b8434f2c31eb4d2826e569ebb4e0e0b29a400dff
    Author: Travis Burke <burke.travis22@gmail.com>
    Date:   Wed Mar 8 17:13:39 2023 -0400
    
        gdgdgdg
    
        gdgdgdg
    
    commit 4e26c917ec323f69d94184f70b77a389ffba0179
    Author: Travis Burke <burke.travis22@gmail.com>
    Date:   Wed Mar 8 16:32:59 2023 -0400
    
        trying to center the bear
    
        ^^
    
    commit 0fbdfcd815eb898b235ebff14d58a5ae1b3b774c
    Author: Travis Burke <burke.travis22@gmail.com>
    Date:   Wed Mar 8 16:20:10 2023 -0400
    
        updates
    
        updades
    
    commit 1e0ebf78e57e9fecbe7bebc7346a6b46a5cd2405
    Author: Travis Burke <burke.travis22@gmail.com>
    Date:   Wed Mar 8 15:47:40 2023 -0400
    
        updated comments
    
        with a question for alex
    
    commit cad6133faf12b685ed049b0853222e2b092c916e
    Author: Travis Burke <burke.travis22@gmail.com>
    Date:   Mon Mar 6 18:56:36 2023 -0400
    
        more on bear
    
        lots of mods I've got it all figured out
    
    commit c038f799e33dc81a122f9c8f254526229a39409c
    Author: Travis Burke <burke.travis22@gmail.com>
    Date:   Mon Mar 6 17:05:50 2023 -0400
    
        fixed comment
    
        js file
    
    commit 482e834695fc1d061afd3cc1a475806568c793f4
    Author: Travis Burke <burke.travis22@gmail.com>
    Date:   Mon Mar 6 16:53:45 2023 -0400
    
        random edits
    
        random
    
    commit b796fc4afcf151e4a6275bd68321c6df209e2246
    Author: Travis Burke <burke.travis22@gmail.com>
    Date:   Mon Mar 6 16:33:33 2023 -0400
    
        Added a Draggable bear
    
        And more
    
    commit 7228d7860a2f72416f0e1c42533c7d22358c335e
    Author: Travis Burke <burke.travis22@gmail.com>
    Date:   Mon Mar 6 16:00:59 2023 -0400
    
        more edits
    
        sizing
    
    commit 2412be9771d8d458a8ecb7956f19a437cd257229
    Author: Travis Burke <burke.travis22@gmail.com>
    Date:   Mon Mar 6 15:37:50 2023 -0400
    
        commenting and one image
    
        :+1:
    
    commit 4bd5cf40d0ecc311cc16de88c10f8b1a9b013014
    Author: Travis Burke <burke.travis22@gmail.com>
    Date:   Mon Mar 6 15:19:23 2023 -0400
    
        fixed link
    
        scss>css
    
    commit 6548b157f3811b2f20a25e3d7bae752c023d183d
    Author: Travis Burke <burke.travis22@gmail.com>
    Date:   Mon Mar 6 14:48:37 2023 -0400
    
        changes
    
        linked style sheet
        changed color

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,87 @@
 /**
  * Generates a random number between 0 and n
- * @param n Any int. Exclusive upper bound of possible numbers to generate 
+ * @param n Any int. Exclusive upper bound of possible numbers to generate
  * @returns x where 0 <= x < n
  * Author: Alex Betschart
  */
 function randomNumber(n) {
-  return Math.floor(Math.random() * n)
+    return Math.floor(Math.random() * n);
+}
+
+//generates random number 1 - 9.
+//Alex I was thinking of integrating this withyou code with the random number generator (above)
+//and we could assign the return value to correct answer and that would help us choose which
+//picture and sound file isthe right one. A new number would be generated after each new game
+// begins.
+window.onload = function randomNumber() {
+    let x = Math.floor(Math.random() * 9) + 1;
+    console.log("The correct answer is : " + x);
+    document.getElementById("correctAnswer").innerHTML = x;
+};
+
+/**
+ * The purpose of this function is to store the id of the element being
+   dragged in a common storage area, under the key "text".
+ *
+ * This function runs as soon as an element has begun to be dragged.   
+ *
+ * @param ev The event being referenced.
+ * Author: Travis Burke
+ */
+function drag(ev) {
+    console.log("drag:" + ev.target.id);
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+/**
+ * The purpose of this function is to suspend the default behaviour so that
+   instead the dragged element can potentially end up with a new position. 
+ * 
+ * This function runs when a dragged element is over a potential target.
+ * 
+ * @param ev The event being referenced.
+ * @param imageNum //needs comment
+ * Author: Travis Burke 
+ */
+function allowDrop(ev, imageNum) {
+    console.log("allowDrop:" + ev.target.id.charAt(1));
+    ev.preventDefault();
+
+    //this is also going to be correctAnswer
+    //needs to be shown after its done
+    console.log("imageNum=" + imageNum);
+    $("#image1").hide();
+}
+
+/**
+ * The purpose of this function is: to allow a dropped element to acquire a
+   new position; retrieve the id of the dropped element using the key "text";
+   and to set the new position of the dropped element; respectively. 
+ * 
+ * This function also compares the id of the dragged element with the id of
+   where the element is dropped.
+ *  
+ * @param ev The event being referenced.
+ * Author: Travis Burke 
+ */
+function drop(ev) {
+    // may need to be edited for 1 dropable.
+    // contains the id of the new location
+    let newLocId = ev.target.id.charAt(1);
+    console.log("drop:" + newLocId);
+
+    ev.preventDefault();
+
+    // contains the id of the element that was being dragged
+    let data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+
+    //this needs to be changed to display the propper image.
+    if (data == newLocId) {
+        //need to make it display button for good job and (star images)
+        console.log("kelu'lk tela'tekn (Good Job)");
+    } else {
+        //need to make it display button for try again and (sunflower images)
+        console.log("tknu'kwalsi ap (Try Again)");
+    }
 }
